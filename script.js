51f766b5ccfdceb2ec6ed296e3cd2a5307559a61
Milanrdnvc <milanradinovic@rocketmail.com>
2020-08-16 13:17:51 +0200

    Implemented terrible winning check

--- script.js
+++ script.js
@@ -10,13 +10,21 @@ function determineTurn(e) {
     if (moves % 2 !== 0) {
         e.target.classList.add('x');
         switchTurnO();
+        if (checkForWin(e.target)) {
+            document.write(checkForWin(e.target));
+        }
 
     } else {
         e.target.classList.add('o');
         switchTurnX();
+        if (checkForWin(e.target)) {
+            document.write(checkForWin(e.target));
+        }
     }
-
     moves++;
+    if (moves > 9) {
+        document.write('draw');
+    }
 }
 
 function switchTurnX() {
@@ -33,3 +41,78 @@ function switchTurnO() {
     });
 }
 
+function checkForWin(cell) {
+    switch (cell.id) {
+        case '1':
+            if (cell.classList[1] === 'x' && ((cells[1].classList.contains('x') && cells[2].classList.contains('x')) || (cells[3].classList.contains('x') && cells[6].classList.contains('x')))) {
+                return 'X wins';
+            } else if (cell.classList[1] === 'o' && ((cells[1].classList.contains('o') && cells[2].classList.contains('o')) || (cells[3].classList.contains('o') && cells[6].classList.contains('o')))) {
+                return 'O wins';
+            }
+            break;
+
+        case '2':
+            if (cell.classList[1] === 'x' && ((cells[0].classList.contains('x') && cells[2].classList.contains('x')) || (cells[4].classList.contains('x') && cells[7].classList.contains('x')))) {
+                return 'X wins';
+            } else if (cell.classList[1] === 'o' && ((cells[0].classList.contains('o') && cells[2].classList.contains('o')) || (cells[4].classList.contains('o') && cells[7].classList.contains('o')))) {
+                return 'O wins';
+            }
+            break;
+
+        case '3':
+            if (cell.classList[1] === 'x' && ((cells[0].classList.contains('x') && cells[1].classList.contains('x')) || (cells[5].classList.contains('x') && cells[8].classList.contains('x')) || (cells[4].classList.contains('x') && cells[6].classList.contains('x')))) {
+                return 'X wins';
+            } else if (cell.classList[1] === 'o' && ((cells[0].classList.contains('o') && cells[1].classList.contains('o')) || (cells[5].classList.contains('o') && cells[8].classList.contains('o')) || (cells[4].classList.contains('o') && cells[6].classList.contains('o')))) {
+                return 'O wins';
+            }
+            break;
+
+        case '4':
+            if (cell.classList[1] === 'x' && ((cells[0].classList.contains('x') && cells[6].classList.contains('x')) || (cells[4].classList.contains('x') && cells[5].classList.contains('x')))) {
+                return 'X wins';
+            } else if (cell.classList[1] === 'o' && ((cells[0].classList.contains('o') && cells[6].classList.contains('o')) || (cells[4].classList.contains('o') && cells[5].classList.contains('o')))) {
+                return 'O wins';
+            }
+            break;
+
+        case '5':
+            if (cell.classList[1] === 'x' && ((cells[1].classList.contains('x') && cells[7].classList.contains('x')) || (cells[3].classList.contains('x') && cells[5].classList.contains('x')) || (cells[2].classList.contains('x') && cells[6].classList.contains('x')) || (cells[0].classList.contains('x') && cells[8].classList.contains('x')))) {
+                return 'X wins';
+            } else if (cell.classList[1] === 'o' && ((cells[1].classList.contains('o') && cells[7].classList.contains('o')) || (cells[3].classList.contains('o') && cells[5].classList.contains('o')) || (cells[2].classList.contains('o') && cells[6].classList.contains('o')) || (cells[0].classList.contains('o') && cells[8].classList.contains('o')))) {
+                return 'O wins';
+            }
+            break;
+
+        case '6':
+            if (cell.classList[1] === 'x' && ((cells[3].classList.contains('x') && cells[4].classList.contains('x')) || (cells[2].classList.contains('x') && cells[8].classList.contains('x')))) {
+                return 'X wins';
+            } else if (cell.classList[1] === 'o' && ((cells[3].classList.contains('o') && cells[4].classList.contains('o')) || (cells[2].classList.contains('o') && cells[8].classList.contains('o')))) {
+                return 'O wins';
+            }
+            break;
+
+        case '7':
+            if (cell.classList[1] === 'x' && ((cells[0].classList.contains('x') && cells[3].classList.contains('x')) || (cells[7].classList.contains('x') && cells[8].classList.contains('x')) || (cells[4].classList.contains('x') && cells[2].classList.contains('x')))) {
+                return 'X wins';
+            } else if (cell.classList[1] === 'o' && ((cells[0].classList.contains('o') && cells[3].classList.contains('o')) || (cells[7].classList.contains('o') && cells[8].classList.contains('o')) || (cells[4].classList.contains('o') && cells[2].classList.contains('o')))) {
+                return 'O wins';
+            }
+            break;
+
+        case '8':
+            if (cell.classList[1] === 'x' && ((cells[6].classList.contains('x') && cells[8].classList.contains('x')) || (cells[1].classList.contains('x') && cells[4].classList.contains('x')))) {
+                return 'X wins';
+            } else if (cell.classList[1] === 'o' && ((cells[6].classList.contains('o') && cells[8].classList.contains('o')) || (cells[1].classList.contains('o') && cells[4].classList.contains('o')))) {
+                return 'O wins';
+            }
+            break;
+
+        case '9':
+            if (cell.classList[1] === 'x' && ((cells[0].classList.contains('x') && cells[4].classList.contains('x')) || (cells[2].classList.contains('x') && cells[5].classList.contains('x')) || (cells[6].classList.contains('x') && cells[7].classList.contains('x')))) {
+                return 'X wins';
+            } else if (cell.classList[1] === 'o' && ((cells[0].classList.contains('o') && cells[4].classList.contains('o')) || (cells[2].classList.contains('o') && cells[5].classList.contains('o')) || (cells[6].classList.contains('o') && cells[7].classList.contains('o')))) {
+                return 'O wins';
+            }
+            break;
+    }
+}
