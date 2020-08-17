34636a5158acb99cf79e616eb3de3c5f09af21a6
Milanrdnvc <milanradinovic@rocketmail.com>
2020-08-17 12:33:41 +0200

    Added bad logic for determining what line to draw

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cells = document.querySelectorAll('.grid__cell');
 const endScreen = document.querySelector('.end-screen');
-const line = document.querySelector('.line');
+const lines = document.querySelectorAll('.line');
 const WINNING_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -75,5 +75,44 @@ function checkForWin(currentClass) {
 
 function drawLine(lineArray) {
     console.log(lineArray);
-    
+    // findIndex
+    if (JSON.stringify(lineArray) === JSON.stringify([0, 1, 2])) {
+        lines[0].classList.add('horizontal-lines');
+        lines[0].classList.add('active');
+    }
+
+    if (JSON.stringify(lineArray) === JSON.stringify([3, 4, 5])) {
+        lines[1].classList.add('horizontal-lines');
+        lines[1].classList.add('active');
+    }
+
+    if (JSON.stringify(lineArray) === JSON.stringify([6, 7, 8])) {
+        lines[2].classList.add('horizontal-lines');
+        lines[2].classList.add('active');
+    }
+
+    if (JSON.stringify(lineArray) === JSON.stringify([0, 3, 6])) {
+        lines[3].classList.add('vertical-lines');
+        lines[3].classList.add('active');
+    }
+
+    if (JSON.stringify(lineArray) === JSON.stringify([1, 4, 7])) {
+        lines[4].classList.add('vertical-lines');
+        lines[4].classList.add('active');
+    }
+
+    if (JSON.stringify(lineArray) === JSON.stringify([2, 5, 8])) {
+        lines[5].classList.add('vertical-lines');
+        lines[5].classList.add('active');
+    }
+
+    if (JSON.stringify(lineArray) === JSON.stringify([0, 4, 8])) {
+        lines[6].classList.add('diagonal-line1');
+        lines[6].classList.add('active');
+    }
+
+    if (JSON.stringify(lineArray) === JSON.stringify([2, 4, 6])) {
+        lines[7].classList.add('diagonal-line2');
+        lines[7].classList.add('active');
+    }
 }
